01706984dbdf1bc0c39f660e1e10587bf83e106f
Luke Archer <P14177187@LEC-ADMIN.dmu.ac.uk>
2017-11-02 14:48:52 +0000

    finished week 5 and added a error message to the js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,16 +2,42 @@
 	let sent = document.getElementById("sentence");
   let sentForm = document.getElementById("sentence-form");
   let space = document.getElementById("space-for-paragraphs");
-	
+  let textstored = localStorage.textst;
+  
+  console.log(`Name on page load: ${textstored}`);
+
+     
 
   function doStuff(ev) {
+	  
+	   if(sent.value === "") {
+      alert("Please enter some text, your last message was " + textstoredmessage());
+      sentForm.focus();
+    }
+	else {
     let para = document.createElement("p");
-    let txt = document.createTextNode(sent.value);
+    let txt = document.createTextNode(sent.value + textstored);
     para.appendChild(txt);
     space.appendChild(para);
     ev.preventDefault();
+	
+	textstored = sent.value;
+	console.log(`Name on page load: ${textstored}`);
+	
+	}
   }
-
+  
+  function textstoredmessage() {
+	  if(textstored === "") {
+		  let message = "no previous message";
+		  return message;
+	  }
+	  else {
+	  return textstored 
+	  }
+  }
+  
+ 
   sentForm.addEventListener("submit", doStuff);
 	
 }());
